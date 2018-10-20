bb532049a17fce948e7a5fa5bd851d08c32e10f7
mariolazzari <mario.lazzari@gmail.com>
2018-10-20 18:23:29 +0200

    Done dir methods

--- directory.js
+++ directory.js
@@ -1,11 +1,26 @@
 const fs = require("fs");
 
 if (fs.existsSync("lib")) {
-  return console.log("lib directory already exists");
+  console.log("lib directory already exists");
 }
 
 fs.mkdir("lib", err => {
-  if (err) return console.log(err);
+  if (err) {
+    console.log(err);
+  } else {
+    console("lib directory created");
+  }
+});
 
-  console("lib directory created");
+// create log directory if not exists
+if (!fs.existsSync("/log")) {
+  fs.mkdir("log", err => {
+    if (err) console.log(err);
+    fs.renameSync("log", "logs");
+  });
+}
+
+// remove dir asynx
+fs.rmdir("notPresent", err => {
+  console.log("Error while removign dir", err);
 });
